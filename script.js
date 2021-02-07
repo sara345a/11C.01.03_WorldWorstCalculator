fb2515f9a9f3d045967b52d976204925973f8dca
Sara Nielsen <sara345a@stud.kea.dk>
2021-02-07 22:20:51 +0100

    different changes, check descriptionbox
    script:
    its now possible to round the results and also clear the 'history'
    
    css:
    just changed some colors, nothing special

--- script.js
+++ script.js
@@ -1,32 +1,120 @@
+"use strict";
+
+//SETTING UP
 window.addEventListener("load", start);
 
+//make 'Clear' clickable and call next function 'clickCalculate'
 function start() {
     console.log("start");
-    document.querySelector("#calculate").addEventListener("click", calc);
+
+document.querySelector("#clear").addEventListener("click", clear);   
+document.querySelector("#calculate").addEventListener("click", calc);
+}
+ 
+
+
+//READ NUMBERS
+function topInput(){
+console.log("topInput");
+
+//read first number and convert from string with 'parseFloat' to a number.
+const numberOne = parseFloat(document.querySelector("#value1").value);
+return numberOne
+}
+    
+
+function bottomInput(){
+console.log("bottomInput");
+    
+//read second number and convert from string with 'parseFloat' to a number.
+const numberTwo = parseFloat(document.querySelector("#value2").value);
+return numberTwo
+}
+    
+
+//READ CALCULATING
+function calc(){
+console.log("calc");
+//collect our numbers
+const inputOne = topInput();
+const inputTwo = bottomInput();
+
+const opr = document.querySelector("#operator").value;
+
+//read operator
+if (opr == "add"){
+calAdd(inputOne, inputTwo);
+}
+else if (opr == "sub"){
+    calSub(inputOne, inputTwo)
+}
+else if (opr == "mul"){
+    calMul(inputOne, inputTwo)
+}
+else if (opr == "div"){
+    calDiv(inputOne, inputTwo)
+}
 }
 
-function calc() {
-	var a = parseInt(document.querySelector("#value1").value);
-	var b = parseInt(document.querySelector("#value2").value);
-	var op = document.querySelector("#operator").value;
-	var calculate;
-	
-	if (op == "add") {
-		calculate = a + b;
-	}
-	else if (op == "sub") { 
-		calculate = a - b;
-	}
-	else if (op == "div") { 
-		calculate = a / b;
-	}
-	else if (op == "mul") { 
-		calculate = a * b;
-	}
-	
-	document.querySelector("#results").innerHTML = calculate;
-    document.querySelector("#results").innerHTML = calculate;
-    document.querySelector("#results").innerHTML = calculate;
+//Add numbers from function'calc'
 
-	
+function calAdd(numberOne, numberTwo){
+const result = numberOne + numberTwo;
+console.log(result);
+
+showResults(result);
+}
+
+function calSub(numberOne, numberTwo){
+    const result = numberOne - numberTwo;
+console.log(result);
+
+showResults(result);
+}
+
+function calMul(numberOne, numberTwo){
+    const result = numberOne * numberTwo;
+console.log(result);
+
+showResults(result);
+}
+
+function calDiv(numberOne, numberTwo){
+    const result = numberOne / numberTwo;
+console.log(result);
+
+showResults(result);
+}
+
+
+//Check if result should be rounded
+function checkIfRound(){
+//check if 'do round'-box is checked
+const checkBox = document.querySelector("#doround").checked;
+return checkBox;
+
+}
+
+
+function showResults(result){
+    console.log("showResults");
+    if(checkIfRound() == true){
+        const decimals = document.querySelector("#decimals").value;
+        const intDecimals = parseInt(decimals);
+        result = result.toFixed(intDecimals);
+    }
+
+//CREATE ELEMENT TO PUT IN 'LI'
+let listEl = document.createElement("li");
+let createString = document.createTextNode(result.toString());
+
+listEl.appendChild(createString);
+document.querySelector("#results").appendChild(listEl);
+document.querySelector("#results").scrollTo(0, 10000000);
+
+}
+
+//MAKE IT POSSIBLE TO CLEAR THE CALCULATOR
+function clear() {
+    document.querySelector("#results").innerHTML = "";
 }
